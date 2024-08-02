16a8d87bc48e922932c3d3989353bbc8fa959bfa
cuiyunxiang <1007568349@qq.com>
2024-08-02 16:23:31 +0800

    cui: asd

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,25 +1,3 @@
-import { RuleConfigSeverity } from '@commitlint/types'
-
 export default {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'type-enum': [
-            RuleConfigSeverity.Error,
-            'always',
-            [
-                'cui',
-                'build',
-                'chore',
-                'ci',
-                'docs',
-                'feat',
-                'fix',
-                'perf',
-                'refactor',
-                'revert',
-                'style',
-                'test',
-            ],
-        ],
-    },
+    extends: ['./packages/commitlint-config/index'],
 }
