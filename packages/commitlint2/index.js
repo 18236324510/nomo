115d09e6a1a0233642898346bf0714efedf68512
cuiyunxiang <1007568349@qq.com>
2024-08-03 13:09:09 +0800

    cui: llll

---
+++ packages/commitlint2/index.js
@@ -0,0 +1,24 @@
+import { RuleConfigSeverity } from '@commitlint/types'
+export default {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [
+            RuleConfigSeverity.Error,
+            'always',
+            [
+                'cui',
+                'build',
+                'chore',
+                'ci',
+                'docs',
+                'feat',
+                'fix',
+                'perf',
+                'refactor',
+                'revert',
+                'style',
+                'test',
+            ],
+        ],
+    },
+}
